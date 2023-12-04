e3150947cee1d3fcdd503e28cb2ec82a31ca4a9b
mehlia <mehliarahman@gmail.com>
2023-12-04 15:07:27 +0000

    displayed countries in API console

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   return (
     <>
       <h1>Country Bucket List</h1>
+      <CountryContainer/>
+
+      <div>
+        <h2>Countries I want to visit</h2>
+        <ul>
+
+        </ul>
+      </div>
+
+      <div>
+        <h2>Countries I have visited</h2>
+      </div>
     </>
   );
 }
--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -3,11 +3,25 @@ import CountryComponent from '../components/CountryComponent';
 
 const CountryContainer = () => {
 
-    const [countries, setCountries] = useState([]);
-    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [countries, setCountries] = useState(null);
+    const [visitedCountries, setVisitedCountries] = useState(null);
+
+    const loadCountriesData = async () => {
+        const response = await fetch ("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        setCountries(data); 
+        console.log(data);
+    }
+
+    useEffect(() => {
+        loadCountriesData();
+    }, []);
+
+    console.log(countries);
 
     return(
         <CountryComponent/>
+
     )
 
 }
